a1e050ed6a3ab1e2c8881dd10b15be01e3989717
Stephen Hussey <shussey@yahoo.com>
2016-11-20 22:45:18 -0800

    router

--- app.js
+++ app.js
@@ -15,9 +15,37 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+
+
+//-- start of routed we'll create our routes here
+// get an instance of router
+var router = express.Router();
+
+// route middleware that will happen on every request
+router.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); 
+});
+
+// home page route (http://localhost:8080)
+router.get('/', function(req, res) {
+    res.send('im the home page!');  
+    //res.send([1,2,3]);
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/about', function(req, res) {
+    res.send('im the about page!'); 
+});
+
+// apply the routes to our application
+app.use('/', router);
+//-- end of routes
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
-
 
 // sample route with a route the way we're used to seeing it
 app.get('/sample', function(req, res) {
